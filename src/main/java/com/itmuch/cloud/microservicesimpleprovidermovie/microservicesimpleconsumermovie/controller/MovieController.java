9aabcbee8f8fcb80cdf87e323a2fbf0a588a3cd7
Administrator <819363948@qq.com>
2019-04-17 21:27:37 +0800

    Initial commit

---
+++ src/main/java/com/itmuch/cloud/microservicesimpleprovidermovie/microservicesimpleconsumermovie/controller/MovieController.java
@@ -0,0 +1,24 @@
+package com.itmuch.cloud.microservicesimpleprovidermovie.microservicesimpleconsumermovie.controller;
+
+import com.itmuch.cloud.microservicesimpleprovidermovie.microservicesimpleconsumermovie.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MovieController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${user.userServiceUrl}")
+    private  String userServiceUrl;
+
+    @GetMapping("/user/{id}")
+    public User findByUser(@PathVariable Long id){
+         return this.restTemplate.getForObject(this.userServiceUrl+id,User.class);
+    }
+}
